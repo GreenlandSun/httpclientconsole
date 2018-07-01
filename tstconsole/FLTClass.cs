7bd2c64894f76e7b889e81de1feaea3c916fe2c8
Kiki Risager <Kiki_Risager@live.dk>
2018-06-30 23:43:26 -0200

    Add project files.

---
+++ tstconsole/FLTClass.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tstconsole
+{
+    public class Rootobject
+    {
+        public string InfoType { get; set; }
+        public string Carrier { get; set; }
+        public int Flight { get; set; }
+        public DateTime OpDate { get; set; }
+        public string REG { get; set; }
+        public string DEP { get; set; }
+        public string ARR { get; set; }
+        public DateTime STD { get; set; }
+        public DateTime STA { get; set; }
+        public int BlockedSeats { get; set; }
+        public Carryin[] CarryIn { get; set; }
+        public Stbid[] StbID { get; set; }
+        public Stbfullprice[] StbFullPrice { get; set; }
+        public Bookedpax[] BookedPAX { get; set; }
+        public Acceptedpax[] AcceptedPAX { get; set; }
+        public Boardedpax[] BoardedPAX { get; set; }
+        public Bookedssr[] BookedSSRs { get; set; }
+        public Acceptedssr[] AcceptedSSRs { get; set; }
+        public Forecastbag[] ForecastBags { get; set; }
+        public Acceptedbag[] AcceptedBags { get; set; }
+        public Bookedspecialbag[] BookedSpecialBags { get; set; }
+        public Acceptedspecialbag[] AcceptedSpecialBags { get; set; }
+        public AC_Config[] AC_Config { get; set; }
+        public Leg_Config[] Leg_Config { get; set; }
+    }
+
+    public class Carryin
+    {
+        public string Carrier { get; set; }
+        public int Flight { get; set; }
+        public Carryinbag[] CarryInBags { get; set; }
+    }
+
+    public class Carryinbag
+    {
+        public string Destination { get; set; }
+        public string BagType { get; set; }
+        public int BagCount { get; set; }
+        public int BagWeight { get; set; }
+    }
+
+    public class Stbid
+    {
+        public string Destination { get; set; }
+        public string Cabin { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+    }
+
+    public class Stbfullprice
+    {
+        public string Destination { get; set; }
+        public string Cabin { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+    }
+
+    public class Bookedpax
+    {
+        public string Destination { get; set; }
+        public string Cabin { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+    }
+
+    public class Acceptedpax
+    {
+        public string Destination { get; set; }
+        public string Cabin { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+    }
+
+    public class Boardedpax
+    {
+        public string Destination { get; set; }
+        public string Cabin { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+    }
+
+    public class Bookedssr
+    {
+        public string Destination { get; set; }
+        public string SSR { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class Acceptedssr
+    {
+        public string Destination { get; set; }
+        public string SSR { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class Forecastbag
+    {
+        public string Destination { get; set; }
+        public string BagType { get; set; }
+        public int BagCount { get; set; }
+        public int BagWeight { get; set; }
+    }
+
+    public class Acceptedbag
+    {
+        public string Destination { get; set; }
+        public string BagType { get; set; }
+        public int BagCount { get; set; }
+        public int BagWeight { get; set; }
+    }
+
+    public class Bookedspecialbag
+    {
+        public string Destination { get; set; }
+        public string BagType { get; set; }
+        public int BagCount { get; set; }
+        public int BagWeight { get; set; }
+    }
+
+    public class Acceptedspecialbag
+    {
+        public string Destination { get; set; }
+        public string BagType { get; set; }
+        public int BagCount { get; set; }
+        public int BagWeight { get; set; }
+    }
+
+    public class AC_Config
+    {
+        public string CabinClass { get; set; }
+        public int Seats { get; set; }
+    }
+
+    public class Leg_Config
+    {
+        public string CabinClass { get; set; }
+        public int Seats { get; set; }
+    }
+}
---
+++ tstconsole/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace tstconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getfltnumber("5").ToString());
+            Console.ReadKey();
+        }
+
+        static int getfltnumber(string flt_number)
+        {
+            string TARGETURL = "https://agfaviobookpusher.azurewebsites.net/Api/Flights/" + flt_number;
+            int i2r = 0;
+            string result = "";
+            try
+            {
+                HttpClientHandler handler = new HttpClientHandler { Credentials = new NetworkCredential("userName", "Password") };
+
+                using (var client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+                    var url = TARGETURL;
+                    var response = client.GetAsync(url).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = response.Content;
+                        //Console.WriteLine(responseContent.Headers.ContentLanguage);
+                        result = responseContent.ReadAsStringAsync().Result;
+
+                        XmlNode myXmlNode = JsonConvert.DeserializeXmlNode(result, "Rootobject");
+                        result = NodeToString(myXmlNode, 2);
+                        //Console.WriteLine(result);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                i2r = -1;
+                Console.WriteLine(e.Message);
+            }
+            i2r = getintfromjson(result, true);
+            return i2r;
+        }
+
+        static string NodeToString(XmlNode node, int indentation)
+        {
+            try
+            {
+                using (var sw = new System.IO.StringWriter())
+                {
+                    using (var xw = new System.Xml.XmlTextWriter(sw))
+                    {
+                        xw.Formatting = System.Xml.Formatting.Indented;
+                        xw.Indentation = indentation;
+                        node.WriteContentTo(xw);
+                    }
+                    return sw.ToString();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Convert to XML error";
+            }
+        }
+
+        static int getintfromjson(string result, bool isxml)
+        {
+            int i2r = 0;
+
+            try
+            {
+                if (isxml)
+                {
+                    //var xmldoc = new XmlDocument();
+                    //xmldoc.LoadXml(result);
+                    //json = JsonConvert.SerializeXmlNode(xmldoc);
+                    i2r = getintfromxml(result);
+                }
+                else
+                {
+                    string json = "";
+                    Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(json);
+                    i2r = obj.Flight;
+                }
+            }
+            catch (Exception e)
+            {
+                i2r = -2;
+                Console.WriteLine(e.Message);
+            }
+
+            return i2r;
+        }
+
+        static int getintfromxml(string result)
+        {
+            int i2r = 0;
+            try
+            {
+                using (TextReader sr = new StringReader(result))
+                {
+                    var serializer = new XmlSerializer(typeof(Rootobject));
+                    Rootobject xml = (Rootobject)serializer.Deserialize(sr);
+                    i2r = xml.Flight;
+                }
+            }
+            catch (Exception e)
+            {
+                i2r = -3;
+                Console.WriteLine(e.Message);
+            }
+
+            return i2r;
+        }
+    }
+}
